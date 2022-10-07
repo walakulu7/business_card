7bd0933d8a0ec35f3658b49d1c0af1f7f201a28f
Janaka Walakuluarachchi <walakulu@gmail.com>
2022-10-07 23:55:05 +0530

    Contact details added

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,16 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Column(
             children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/avetar.jpg'),
+              ),
               Text(
                 'Janaka Walakulu',
                 style: TextStyle(
                   fontFamily: 'Pacifico',
                   fontSize: 40.0,
-                  color: Colors.yellow,
+                  color: Colors.white,
                   fontWeight: FontWeight.bold,
                 ),
               ),
@@ -31,6 +35,60 @@ class MyApp extends StatelessWidget {
                   fontSize: 20.0,
                   color: Colors.teal.shade100,
                   letterSpacing: 2.5,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+94 771 234 567',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'walakulu@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        color: Colors.teal.shade900,
+                        fontSize: 20.0,
+                      ),
+                    )
+                  ],
                 ),
               )
             ],
